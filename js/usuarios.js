2fa785ae54e88ef512378803eb927c0a3e89e769
infopocos <gustavofortunatocorrea@gmail.com>
2025-07-25 08:59:35 -0300

    Create usuarios.js

---
+++ js/usuarios.js
@@ -0,0 +1,17 @@
+// Lista de usuários e permissões
+const usuarios = {
+  "admin": {
+    senha: "admin123",
+    tipo: "admin"
+  },
+  "arco-iris": {
+    senha: "arco123",
+    tipo: "cliente",
+    pasta: "arco-iris"
+  },
+  "pain": {
+    senha: "pain123",
+    tipo: "cliente",
+    pasta: "pain"
+  }
+};
